31cfb9bae46b1d22b98c6054cecbe6e62286ab52
MartinXCVI <mdzg96@gmail.com>
2024-10-31 23:19:59 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+// We define Express
+const express = require('express')
+// We define our app
+const app = express()
+/* We import the 'path' module to provide utilities 
+for working with file and directory paths */
+const path = require('node:path')
+// We define the constant for the port
+const PORT = process.env.PORT || 3500
+
+// We provide our app the route to look for static files
+app.use('/', express.static(path.join(__dirname, '/public')))
+
+// We require the root of our router
+app.use('/', require('./routes/root'))
+
+// Redirection to 'error' page
+app.all('*', (req, res)=> {
+  res.status(404)
+  if(req.accepts('html')) {
+    // HTML response
+    res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } else if(req.accepts('json')) {
+    // JSON response
+    res.json({ message: '404 - Not found' })
+  } else {
+    // Plain text response
+    res.type('txt').send('404 - Not found')
+  }
+})
+
+// We order our app to start listening
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}...`))
