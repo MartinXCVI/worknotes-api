418d463b9a9602f000cac64bca90d69754a81827
MartinXCVI <mdzg96@gmail.com>
2024-11-05 02:11:21 -0300

    Added 2 more users controllers: updateUser controller, deleteUser controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -67,7 +67,43 @@ const createNewUser = asyncHandler(async (req, res)=> {
 // @method: PATCH
 // @access: Private
 const updateUser = asyncHandler(async (req, res)=> {
-  
+  // Destructuring the data we'll receive from the frontend
+  const { id, username, roles, active, password } = req.body
+
+  // Confirming data
+  if(!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean') {
+    return res.status(400).json({ message: "All fields are required" })
+  }
+
+  // Defining our user
+  // Executing the query with exec() to return a promise
+  const user = await User.findById(id).exec()
+
+  if(!user) {
+    return res.status(400).json({ message: 'User not found' })
+  }
+
+  // Checking for duplicate
+  const duplicate = await User.findOne({ username }).lean().exec()
+
+  // Allow updates to the original user
+  if(duplicate && duplicate?._id.toString() !== id) {
+    return res.status(409).json({ message: 'Duplicate username' })
+  }
+
+  // Updating our user object
+  user.username = username
+  user.roles = roles
+  user.active = active
+
+  if(password) {
+    // Hash password
+    user.password = await bcrypt.hash(password, 10) // salt rounds
+  }
+
+  // Update of our user
+  const updatedUser = await user.save()
+  res.json({ message: `${updatedUser.username} updated!` })
 })
 
 // @description: Delete a user
@@ -75,7 +111,28 @@ const updateUser = asyncHandler(async (req, res)=> {
 // @method: DELETE
 // @access: Private
 const deleteUser = asyncHandler(async (req, res)=> {
-  
+  const { id } = req.body // Only destructuring the id
+
+  if(!id) {
+    return res.status(400).json({ message: 'User ID is required' })
+  }
+
+  // preventing the deletion of users with assigned notes
+  const notes = await Note.findOne({ user: id }).lean().exec()
+  if(notes?.length) {
+    return res.status(400).json({ message: 'User currently has assigned notes' })
+  }
+
+  const user = await User.findById(id).exec()
+
+  if(!user) {
+    return res.status(400).json({ message: 'User not found' })
+  }
+  // The full user object that is deleted
+  const result = await user.deleteOne()
+  // Delete message
+  const reply = `Username ${result.username} with ID ${result.id} has been successfully deleted`
+  res.json(reply)
 })
 
 module.exports = {
